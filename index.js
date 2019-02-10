a5d256ffd884371c6a8da66ccb90d5e6d58ac3fa
Josef Lexa <lexajosef94@gmail.com>
2019-02-10 16:51:09 +0100

    Add loging of requests and edit index file

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
 const config = require('config');
 const express = require('express');
 const bearerToken = require('express-bearer-token');
-const auth = require('./middleware/authorization');
+const morgan = require('morgan');
 const app = express();
 
 app.use(bearerToken());
+app.use(morgan('dev'));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-require('./startup/routes')(app);
 
-/*app.get('/', (req, res) => {
-  console.log(req.user);
-  res.send('Hello world!');
-});
-app.get('/world', auth, (req, res) => {
-  console.log(req.user);
-  res.send('The Secret Hello World!');
-});*/
+require('./startup/routes')(app);
 
 const port = process.env.PORT || config.get('port');
 app.listen(port, () => { 
